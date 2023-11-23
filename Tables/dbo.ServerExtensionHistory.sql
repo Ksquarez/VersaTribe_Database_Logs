1653bdf6f945e7ee82899e0149b06beb440e270a
KSGS\shubhamb <shubham@gmail.com>
2023-11-23 15:50:43 +0530

    Delete_those_tables

--- Tables/dbo.ServerExtensionHistory.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[ServerExtensionHistory] (
-  [Server_Id] [int] NOT NULL,
-  [Ext_Id] [int] NOT NULL,
-  [Status] [int] NOT NULL,
-  [TStamp] [datetime2] NOT NULL,
-  [TOwner] [nvarchar](256) NOT NULL
-)
-ON [PRIMARY]
-GO
--- Tables/dbo.ServersHistory.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[ServersHistory] (
-  [Server_Id] [int] NOT NULL,
-  [Server_Name] [nvarchar](50) NOT NULL,
-  [Status] [int] NOT NULL,
-  [TStamp] [datetime2] NOT NULL,
-  [TOwner] [nvarchar](256) NOT NULL
-)
-ON [PRIMARY]
-GO
