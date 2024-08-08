a8ca38925d687645d07960de4dda2de06ed2dbab
KSGS\shubhamb <shubham@gmail.com>
2024-08-08 14:46:38 +0530

    -- Add trunks amd trunks_prefix

---
+++ Tables/dbo.trunk.sql
@@ -0,0 +1,33 @@
+﻿CREATE TABLE [dbo].[trunk] (
+  [Id] [int] IDENTITY,
+  [context_Id] [int] NULL,
+  [trunk_name] [nvarchar](20) NULL,
+  [trunk_server_ip] [nvarchar](max) NULL,
+  [trunk_server_port] [nvarchar](50) NULL,
+  [trunk_authentication] [bit] NULL,
+  [trunk_password] [nvarchar](max) NULL,
+  [trunk_username] [nvarchar](max) NULL,
+  [Status] [int] NULL,
+  [TOwner] [nvarchar](max) NULL,
+  [TStamp] [datetime] NULL,
+  [Org_Id] [int] NULL,
+  [Active] [bit] NULL,
+  [server_id] [int] NULL,
+  CONSTRAINT [PK_trunk_Id] PRIMARY KEY CLUSTERED ([Id])
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
+
+CREATE UNIQUE INDEX [UK_trunk_trunk_name]
+  ON [dbo].[trunk] ([trunk_name])
+  ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[trunk]
+  ADD FOREIGN KEY ([context_Id]) REFERENCES [dbo].[context] ([Id])
+GO
+
+ALTER TABLE [dbo].[trunk]
+  ADD FOREIGN KEY ([Org_Id]) REFERENCES [dbo].[Org] ([Org_Id])
+GO
---
+++ Tables/dbo.trunk_prefix.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE [dbo].[trunk_prefix] (
+  [Id] [int] IDENTITY,
+  [Org_Id] [int] NULL,
+  [trunk_Id] [int] NULL,
+  [trunk_prefix_name] [nvarchar](10) NULL,
+  [Status] [int] NULL,
+  [TStamp] [datetime] NULL,
+  [TOwner] [nvarchar](max) NULL,
+  [Active] [bit] NULL,
+  CONSTRAINT [PK_trunk_prefix_Id] PRIMARY KEY CLUSTERED ([Id])
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
+
+CREATE UNIQUE INDEX [IDX_trunk_prefix]
+  ON [dbo].[trunk_prefix] ([trunk_Id], [trunk_prefix_name])
+  ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[trunk_prefix]
+  ADD FOREIGN KEY ([Org_Id]) REFERENCES [dbo].[Org] ([Org_Id])
+GO
+
+ALTER TABLE [dbo].[trunk_prefix]
+  ADD FOREIGN KEY ([trunk_Id]) REFERENCES [dbo].[trunk] ([Id])
+GO
